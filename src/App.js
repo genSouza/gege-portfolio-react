6448d6ec9ff00f7e3dbcb0f5d852898852e2e78c
Genilton Souza <genilton.souza26@gmail.com>
2022-08-25 15:49:49 -0400

    Add contact session

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Experience from "./components/Experience/Experience";
 import Home from "./components/Home/Home";
 import NavBar from "./components/Navbar/NavBar";
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Portfolio />
       <Experience />
+      <Contact />
       <SocialLinks />
     </div>
   );
